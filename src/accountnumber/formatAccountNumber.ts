cb6a81fb48402b985983aba12ac5b96d4249435e
bim.krismon <bim.krismon@permatabank.co.id>
2026-05-04 13:08:46 +0700

    [features][format-account-nummber]: enhance formater linter and prettier

--- src/accountnumber/formatAccountNumber.ts
+++ src/accountnumber/formatAccountNumber.ts
@@ -2,14 +2,14 @@ export function formatAccountNumber(
   acctNum: string,
   separator: string = '-'
 ): string {
-  if (!acctNum) return '';
+  if (!acctNum) return ''
 
-  const onlyDigit = acctNum.replace(/\D/g, ''); //ignore character non digit 0-9
-  if (!onlyDigit) return '';
+  const onlyDigit = acctNum.replace(/\D/g, '') //ignore character non digit 0-9
+  if (!onlyDigit) return ''
 
-  const escapedSeparator = separator.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  const escapedSeparator = separator.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
 
   return onlyDigit
     .replace(/(.{4})/g, `$1${separator}`)
-    .replace(new RegExp(`${escapedSeparator}$`), '');
+    .replace(new RegExp(`${escapedSeparator}$`), '')
 }
--- tests/formatAccountNumber.test.ts
+++ tests/formatAccountNumber.test.ts
@@ -1,26 +1,25 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it } from 'vitest'
 
 import { formatAccountNumber } from '../src/accountnumber'
 
 describe('format account number', () => {
-    it('should format account number with validate digits', () => {
-        expect(formatAccountNumber('1234567890')).toBe('1234-5678-90')
-    })
+  it('should format account number with validate digits', () => {
+    expect(formatAccountNumber('1234567890')).toBe('1234-5678-90')
+  })
 
-    it('should format mixed digits and non digits', () => {
-        expect(formatAccountNumber('cool2149itu42nda4912')).toBe('2149-4249-12')
-    })
+  it('should format mixed digits and non digits', () => {
+    expect(formatAccountNumber('cool2149itu42nda4912')).toBe('2149-4249-12')
+  })
 
-    it('should format with separator', () => {
-        expect(formatAccountNumber('1234-5678-9120')).toBe('1234-5678-9120')
-    })
+  it('should format with separator', () => {
+    expect(formatAccountNumber('1234-5678-9120')).toBe('1234-5678-9120')
+  })
 
-    it('should format whitout digits', () => {
-        expect(formatAccountNumber('permudah jalankan menujuMu ya Rabb')).toBe('')
-    })
+  it('should format whitout digits', () => {
+    expect(formatAccountNumber('permudah jalankan menujuMu ya Rabb')).toBe('')
+  })
 
-    it('should formate whit space', () => {
-        expect(formatAccountNumber('1234 5678 9123',)).toBe('1234-5678-9123')
-    })
-
+  it('should formate whit space', () => {
+    expect(formatAccountNumber('1234 5678 9123')).toBe('1234-5678-9123')
+  })
 })

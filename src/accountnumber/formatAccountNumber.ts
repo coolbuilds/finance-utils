e74f21dc446589e6e7c8b2a615f46f54b7ac337b
bim.krismon <bim.krismon@permatabank.co.id>
2026-05-04 12:59:32 +0700

    feat: create formater account number

---
+++ src/accountnumber/formatAccountNumber.ts
@@ -0,0 +1,15 @@
+export function formatAccountNumber(
+  acctNum: string,
+  separator: string = '-'
+): string {
+  if (!acctNum) return '';
+
+  const onlyDigit = acctNum.replace(/\D/g, ''); //ignore character non digit 0-9
+  if (!onlyDigit) return '';
+
+  const escapedSeparator = separator.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+  return onlyDigit
+    .replace(/(.{4})/g, `$1${separator}`)
+    .replace(new RegExp(`${escapedSeparator}$`), '');
+}
